bdc9641f3231894fd820f1e3eef050bb50976a72
Deepanshu <djain1309@gmail.com>
2023-11-16 05:42:00 -0500

    Created a header-to learn component interaction

---
+++ src/app/Header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-header',
+    template : '<h3>Learnen Angular</h3>'
+})
+
+// Without Decorator (@Component) : below is just a simple typescript class
+// Using Decorator: it becomes a component
+export class HeaderComponent {
+
+}
